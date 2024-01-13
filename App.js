0e7ab945487635ee3209751dffdc24d6c3118826
Douglas Silva <douglassilvaaraujo78@gmail.com>
2024-01-13 14:33:27 -0300

    add: Creating the application's APP and using the pages created

---
+++ App.js
@@ -0,0 +1,31 @@
+import { StyleSheet, StatusBar, Text, View, SafeAreaView, Platform } from 'react-native';
+
+import { CurrentPrice } from './src/components/CurrentPrice';
+import { HistoryGraphic } from './src/components/HistoryGraphic';
+import { QuotationList } from './src/components/QuotationsList';
+import { QuotationItems } from './src/components/QuotationsList/QuotationsItems';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar
+        backgroundColor='#fff'
+        barStyle="dark-content"
+      />
+      
+      <CurrentPrice/>
+      <HistoryGraphic/>
+      <QuotationList/>
+      <QuotationItems/>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    paddingTop: Platform.OS === "android" ? 40 : 0
+  },
+});
