b28d46af4a198211212ea0efd487edb583ac8a4e
Douglas Silva <douglassilvaaraujo78@gmail.com>
2024-01-13 14:29:58 -0300

    add: Creating the page: HistoryGraphic

---
+++ src/components/HistoryGraphic/index.js
@@ -0,0 +1,9 @@
+import { View, Text  } from "react-native";
+import styles from './style';
+
+export const HistoryGraphic = () =>{
+    return(
+        <View style={styles.contentGraphic}>
+        </View>
+    );
+}
