793edba8ba13d5cd067bbd809c1e17f7b9e715d9
Douglas Silva <douglassilvaaraujo78@gmail.com>
2024-01-14 09:21:46 -0300

    refacto: Updating the page: CurrentPrice

--- src/components/CurrentPrice/index.js
+++ src/components/CurrentPrice/index.js
@@ -1,10 +1,10 @@
 import { View, Text  } from "react-native";
 import styles from './style';
 
-export const CurrentPrice = () =>{
+export const CurrentPrice = ({price}) =>{
     return(
         <View style={styles.headerPrice}>
-            <Text style={styles.currentPrice}>$ 54423.365</Text>
+            <Text style={styles.currentPrice}>$ {price}</Text>
             <Text style={styles.textPrice}>Última cotação</Text>
         </View>
     );
