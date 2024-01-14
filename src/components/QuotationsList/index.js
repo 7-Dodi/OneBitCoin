f89a0d218a15e805f103a5e26573e3edc69d3c9b
Douglas Silva <douglassilvaaraujo78@gmail.com>
2024-01-14 09:23:11 -0300

    refactor: Updating the page: QuotationsList

--- src/components/QuotationsList/index.js
+++ src/components/QuotationsList/index.js
@@ -1,45 +1,52 @@
-import { View, Text, ScrollView, TouchableOpacity  } from "react-native";
+import { View, Text, ScrollView, TouchableOpacity, FlatList  } from "react-native";
 import styles from './style';
 import { Fragment } from "react";
+import { QuotationItems } from "./QuotationsItems";
 
-export const QuotationList = () =>{
+export const QuotationList = ({filterDay, listTransactions}) =>{
+    const daysQuery = filterDay
     return(
         <Fragment>
         <View style={styles.filters}>
             <TouchableOpacity
                 style={styles.buttonQuery}
-                onPress={()=> {}}    
+                onPress={()=> daysQuery(7)}    
             >
                 <Text style={styles.textBuutonQuery}>7D</Text>
             </TouchableOpacity>
 
             <TouchableOpacity
                 style={styles.buttonQuery}
-                onPress={()=> {}}    
+                onPress={()=> daysQuery(15)}    
             >
                 <Text style={styles.textBuutonQuery}>15D</Text>
             </TouchableOpacity>
 
             <TouchableOpacity
                 style={styles.buttonQuery}
-                onPress={()=> {}}    
+                onPress={()=> daysQuery(30)}    
             >
                 <Text style={styles.textBuutonQuery}>1M</Text>
             </TouchableOpacity>
 
             <TouchableOpacity
                 style={styles.buttonQuery}
-                onPress={()=> {}}    
+                onPress={()=> daysQuery(90)}    
             >
                 <Text style={styles.textBuutonQuery}>3M</Text>
             </TouchableOpacity><TouchableOpacity
                 style={styles.buttonQuery}
-                onPress={()=> {}}    
+                onPress={()=> daysQuery(180)}    
             >
                 <Text style={styles.textBuutonQuery}>6M</Text>
             </TouchableOpacity>
         </View>
-        <ScrollView></ScrollView>
+            <FlatList 
+                data={listTransactions}
+                renderItem={({item}) => {
+                    return <QuotationItems valor={item.valor} data={item.data}/>
+                }}
+            />
         </Fragment>
     );
 }
