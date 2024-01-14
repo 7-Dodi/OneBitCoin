3cd36d7707bab77eb2c39edd9bb1d537cb1ce399
Douglas Silva <douglassilvaaraujo78@gmail.com>
2024-01-14 09:22:14 -0300

    refactor: Updating the page: HistoryGraphic

--- src/components/HistoryGraphic/index.js
+++ src/components/HistoryGraphic/index.js
@@ -1,9 +1,44 @@
-import { View, Text  } from "react-native";
+import { View, Dimensions } from "react-native";
+import { LineChart } from "react-native-chart-kit"
 import styles from './style';
 
-export const HistoryGraphic = () =>{
-    return(
-        <View style={styles.contentGraphic}>
+export const HistoryGraphic = ({ infoDataGraphic }) => {
+    const dataValues = infoDataGraphic.map(item => item.valor);
+
+
+    return (
+        <View>
+            <LineChart
+                data={{
+                    datasets: [
+                        {
+                            data: dataValues
+                        }
+                    ]
+
+                }}
+                width={Dimensions.get("window").width}
+                height={220}
+                yAxisLabel="$"
+                yAxisSuffix="k"
+                withVerticalLines={false}
+                yLabelsOffset={1}
+                withVerticalLabels={false}
+                chartConfig={{
+                    backgroundColor: "#000",
+                    backgroundGradientFrom: "#232323",
+                    backgroundGradientTo: "#3F3F3F",
+                    decimalPlaces: 0,
+                    color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                    labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                    propsForDots: {
+                        r: "1",
+                        strokeWidth: "1",
+                        stroke: "#f50d41",
+                    }
+                }}
+                bezier
+            />
         </View>
     );
 }
